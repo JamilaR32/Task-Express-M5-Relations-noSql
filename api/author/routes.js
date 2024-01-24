aeee73ab8966ca9a02589c3b5a65d5783a5e9c89
Jamila <j.r.cba@hotmail.com>
2024-01-25 00:47:37 +0300

    con.

--- api/author/routes.js
+++ api/author/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { postsCreate, getAllAuthors, newAuthor } = require("./controllers");
 
-router.post("/posts", postsCreate);
+router.post("/posts/:authorId", postsCreate);
 router.get("/", getAllAuthors);
 router.post("/", newAuthor);
 module.exports = router;
